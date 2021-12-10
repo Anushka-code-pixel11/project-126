e02277a2ba2841e66aa00ee1d8c6e0355c7c2ac8
Anushka-code-pixel11 <71310610+Anushka-code-pixel11@users.noreply.github.com>
2021-12-10 17:52:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import PickImage from "./camera";
+
+export default function App() {
+  return (
+    <PickImage />
+  );
+}
+
+
